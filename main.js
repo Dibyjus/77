5291abfd5d2ce837bcf850503e8e9f16bc38c1fe
Dibyjus <113060789+Dibyjus@users.noreply.github.com>
2023-02-07 16:56:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Meu primeiro projeto no console");
+var firstname =("Diógenes")
+var lastname =(" Filho")
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(add);
+var sub = a - b ;
+console.log(sub);
+var multiple = a * b ;
+console.log(multiple);
+var divide = a / b ;
+console.log(divide);
